0539294859fe41fc7e1552c0972a3d6687a9e1e6
DmitryOshkin <46111421+DmitryOshkin@users.noreply.github.com>
2020-09-17 16:39:25 +0300

    Удалены тренировочные строки

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -5,41 +5,9 @@ import java.sql.SQLOutput;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    System.out.println("Hello, world!");
 
-    System.out.println(2 + 2);
-    System.out.println(2 * 2);
-    System.out.println(2 / 2);
-    System.out.println(2 - 2);
-
-    System.out.println(1 / 2);
-    System.out.println(1.0 / 2);
-    System.out.println(1 / 2.0);
-    System.out.println(2.0 / 2);
-
-    System.out.println("2" + "2"); //конкатенация
-    System.out.println("2" + 2);
-    System.out.println(2 + "2");
-
-    System.out.println(2 + 2 * 2);
-    System.out.println((2 + 2) * 2);
-    System.out.println("2 + 2 = " + 2 +2);
-    System.out.println("2 + 2 = " + (2 +2));
-
-    System.out.println("Hello, " + "world!");
-
-    System.out.println("Площадь квадрата со стороной " + 5 + " = " + (5 * 5));
-    System.out.println("Площадь квадрата со стороной " + 6 + " = " + (6 * 6));
-    int l = 8;
-    System.out.println("Площадь квадрата со стороной " + l + " = " + (l * l));
-    int s = l * l;
-    System.out.println("Площадь квадрата со стороной " + l + " = " + s);
-
-
-    double L = 8.0;
-    double S = L * L;
-    String somebody = "world";
-    System.out.println("Hello, " + somebody + "!");
+    
 
   }
 
