2a925bbf5227ce65e6864a012eeea9a5840b65da
DmitryOshkin <46111421+DmitryOshkin@users.noreply.github.com>
2020-09-17 17:09:34 +0300

    Добавлены несколько функций

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -5,10 +5,30 @@ import java.sql.SQLOutput;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
+    hello("Humans"); //Вызываем функцию передавая в нее параметр
+    hello("world");
+    hello("Dmitry");
+    double l = 11;
+    System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
 
-    
+    double a = 4;
+    double b = 6;
+    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
 
+
+  }
+
+  public static void hello(String somebody) { //Создаем функцию с параметром somebody
+    System.out.println("Hello, " + somebody + "!");
+
+  }
+
+  public static double area (double len) {
+    return len * len;
+
+  }
+  public static double area (double a, double b) {
+    return a * b;
   }
 
 }
