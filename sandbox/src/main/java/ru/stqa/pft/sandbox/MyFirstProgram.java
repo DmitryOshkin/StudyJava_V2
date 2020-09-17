ae3dacadc6979bff7d317ea28ea6601d152b3fc0
DmitryOshkin <46111421+DmitryOshkin@users.noreply.github.com>
2020-09-17 18:04:09 +0300

    Перенес функции внутрь классов

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -16,11 +16,10 @@ public class MyFirstProgram {
     System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
 
     Square s = new Square(5);
-    System.out.println("Площадь квадрата со стороной " + s.l + " = " + area(s));
+    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
     Rectangle r = new Rectangle(4 ,6 );
-
-    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area(r));
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 
   }
 
@@ -37,12 +36,8 @@ public class MyFirstProgram {
     return a * b;
   }
 
-  public static double area(Square s) {
-    return s.l * s.l;
-  }
 
-  public static double area (Rectangle r) {
-    return r.a * r.b;
-  }
+
+
 
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -9,4 +9,9 @@ public class Square {
     this.l = l;
 
   }
+
+  public double area() {
+    return this.l * this.l;
+  }
+
 }
