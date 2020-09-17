ee60f05be3860852cb85f48ff17bf62e9a21e5d1
DmitryOshkin <46111421+DmitryOshkin@users.noreply.github.com>
2020-09-17 14:44:40 +0300

    Примеры переменных и значений

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,5 +1,7 @@
 package ru.stqa.pft.sandbox;
 
+import java.sql.SQLOutput;
+
 public class MyFirstProgram {
 
   public static void main(String[] args) {
@@ -23,6 +25,15 @@ public class MyFirstProgram {
     System.out.println((2 + 2) * 2);
     System.out.println("2 + 2 = " + 2 +2);
     System.out.println("2 + 2 = " + (2 +2));
+
+    System.out.println("Hello, " + "world!");
+
+    System.out.println("Площадь квадрата со стороной " + 5 + " = " + (5 * 5));
+    System.out.println("Площадь квадрата со стороной " + 6 + " = " + (6 * 6));
+    int l = 8;
+    System.out.println("Площадь квадрата со стороной " + l + " = " + (l * l));
+    int s = l * l;
+    System.out.println("Площадь квадрата со стороной " + l + " = " + s);
 
   }
 
