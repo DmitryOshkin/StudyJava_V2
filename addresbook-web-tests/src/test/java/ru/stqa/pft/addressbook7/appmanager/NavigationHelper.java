eb074e3b50f7b231541327bd55b2e4b09bb6e69c
DmitryOshkin <46111421+DmitryOshkin@users.noreply.github.com>
2020-09-20 19:37:49 +0300

    Оптимизированы переходы между страницами. Если уже находимся на нужно странице доп действий делать не нужно.

--- addresbook-web-tests/src/test/java/ru/stqa/pft/addressbook7/appmanager/NavigationHelper.java
+++ addresbook-web-tests/src/test/java/ru/stqa/pft/addressbook7/appmanager/NavigationHelper.java
@@ -11,10 +11,18 @@ public class NavigationHelper extends HelperBase {
   }
 
   public void gotoGroupPage() {
+    if (isElementPresent(By.tagName("h1"))                                 //Проверяем наличие заголовка "h1" на странице
+            && wd.findElement(By.tagName("h1")).getText().equals("Groups") //Далее ищем элемент по локатору, берем его текст и сверяем его содержимое методом equals со значением Groups
+            && isElementPresent(By.name("new"))) {                         //Проверяем наличие кнопки создания группы
+      return;
+    }
     click(By.linkText("groups"));
   }
 
   public void gotoHomePage() {
+    if (isElementPresent(By.id("maintable"))) { // Проверка наличия элемента таблица на странице
+      return;
+    }
     click(By.linkText("home"));
   }
 }
