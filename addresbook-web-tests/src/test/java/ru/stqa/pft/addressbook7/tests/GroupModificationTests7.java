aa6022132b8de749aac7aa123168b7fa026c6af6
DmitryOshkin <46111421+DmitryOshkin@users.noreply.github.com>
2020-09-21 17:42:24 +0300

    Модифицировал метод выбора элемента на странице выбора групп, теперь выбор элементов осуществляется по индексу

--- addresbook-web-tests/src/test/java/ru/stqa/pft/addressbook7/tests/GroupModificationTests7.java
+++ addresbook-web-tests/src/test/java/ru/stqa/pft/addressbook7/tests/GroupModificationTests7.java
@@ -13,7 +13,7 @@ public class GroupModificationTests7 extends TestBase {
     if (! app.getGroupHelper().isThereAGroup()) {   //Добавляем условие для проверки наличия группы
       app.getGroupHelper().createGroup(new GroupData("test1", null, null));
     }
-    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().selectGroup(before -1);
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test6"));
     app.getGroupHelper().submitGroupModification();
