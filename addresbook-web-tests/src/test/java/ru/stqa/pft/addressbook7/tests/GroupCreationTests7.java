522459c9053dae33c212f0b8b0729508bad748c4
DmitryOshkin <46111421+DmitryOshkin@users.noreply.github.com>
2020-09-21 21:50:27 +0300

    Модифицировал вычисление максимального идентификатора в коллекции, пуутем вычисления через лямбда выражение.

--- addresbook-web-tests/src/test/java/ru/stqa/pft/addressbook7/tests/GroupCreationTests7.java
+++ addresbook-web-tests/src/test/java/ru/stqa/pft/addressbook7/tests/GroupCreationTests7.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook7.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -14,18 +15,12 @@ public class GroupCreationTests7 extends TestBase {
   public void testGroupCreation() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
     List<GroupData> before = app.getGroupHelper().getGroupList();
-    GroupData group = new GroupData("test1", null, null);
+    GroupData group = new GroupData("test2", null, null);
     app.getGroupHelper().createGroup(group);
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() + 1);
 
-    int max = 0;                                                                   //Найти максимальный идентификатор из всех в списке
-    for (GroupData g : after) {                                                    //Проходим по всем элементам списка after
-      if (g.getId() > max) {
-        max =g.getId();
-      }
-    }
-    group.setId(max);                                                              //Присваиваем максимальный элемент в качестве идетификатора новой группы
+    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());    //Вычисление максимального идентификатора через лямбда выражение.                                                          //Присваиваем максимальный элемент в качестве идетификатора новой группы
     before.add(group);                                                             //Добавляем в список уже модифицированный элемент
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));  //Перед сравнением преобразовываем списки в множества
   }
