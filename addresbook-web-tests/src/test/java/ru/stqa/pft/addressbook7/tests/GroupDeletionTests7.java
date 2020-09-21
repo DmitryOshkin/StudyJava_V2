889378a9bad8ce148d646e59a4ae8153c0d3f12d
DmitryOshkin <46111421+DmitryOshkin@users.noreply.github.com>
2020-09-21 16:36:19 +0300

    Создал метод для подсчета количества элементов на странице, и добавил проверки подсчета количества элементов до и после выполнения тестов для групп,

--- addresbook-web-tests/src/test/java/ru/stqa/pft/addressbook7/tests/GroupDeletionTests7.java
+++ addresbook-web-tests/src/test/java/ru/stqa/pft/addressbook7/tests/GroupDeletionTests7.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook7.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook7.model.GroupData;
 
@@ -9,12 +10,15 @@ public class GroupDeletionTests7 extends TestBase {
   @Test
   public void testGroupDeletion() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
+    int before = app.getGroupHelper().getGroupCount();
     if (! app.getGroupHelper().isThereAGroup()) {   //Добавляем условие для проверки наличия группы
       app.getGroupHelper().createGroup(new GroupData("test1", null, null));
     }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
+    int after = app.getGroupHelper().getGroupCount();
+    Assert.assertEquals(after,before - 1);
   }
 
 
--- addresbook-web-tests/src/test/java/ru/stqa/pft/addressbook7/tests/GroupModificationTests7.java
+++ addresbook-web-tests/src/test/java/ru/stqa/pft/addressbook7/tests/GroupModificationTests7.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook7.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook7.model.GroupData;
 
@@ -8,6 +9,7 @@ public class GroupModificationTests7 extends TestBase {
   @Test
   public void testGroupModification() {
     app.getNavigationHelper().gotoGroupPage();
+    int before = app.getGroupHelper().getGroupCount();
     if (! app.getGroupHelper().isThereAGroup()) {   //Добавляем условие для проверки наличия группы
       app.getGroupHelper().createGroup(new GroupData("test1", null, null));
     }
@@ -16,6 +18,7 @@ public class GroupModificationTests7 extends TestBase {
     app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test6"));
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
-
+    int after = app.getGroupHelper().getGroupCount();
+    Assert.assertEquals(after,before);
   }
 }
