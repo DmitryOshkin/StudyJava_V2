d39e4cf3d2c251d8826d24d7b0a217f87fc8d8d8
DmitryOshkin <46111421+DmitryOshkin@users.noreply.github.com>
2020-09-21 18:55:08 +0300

    Добавлена проверка для сравнения списков групп целиком до и после выполнения удаления группы.

--- addresbook-web-tests/src/test/java/ru/stqa/pft/addressbook7/model/GroupData.java
+++ addresbook-web-tests/src/test/java/ru/stqa/pft/addressbook7/model/GroupData.java
@@ -1,5 +1,7 @@
 package ru.stqa.pft.addressbook7.model;
 
+import java.util.Objects;
+
 public class GroupData {
   private final String name;
   private final String header;
@@ -21,5 +23,25 @@ public class GroupData {
 
   public String getFooter() {
     return footer;
+  }
+
+  @Override
+  public String toString() {                            // Преобразование атрибута объекта в строку. Текстовое представление элемента
+    return "GroupData{" +
+            "name='" + name + '\'' +
+            '}';
+  }
+
+  @Override
+  public boolean equals(Object o) {                               // Метод для сравнения объектов типа GroupData
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    GroupData groupData = (GroupData) o;
+    return Objects.equals(name, groupData.name);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(name);
   }
 }
