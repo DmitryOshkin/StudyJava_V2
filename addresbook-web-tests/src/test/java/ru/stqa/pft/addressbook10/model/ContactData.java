72224b61dea9bbb468a6b1d7895a309d9bfba4ea
DmitryOshkin <46111421+DmitryOshkin@users.noreply.github.com>
2020-10-03 15:06:07 +0300

    7.2 7.3 ORM и Условия отбора данных

--- addresbook-web-tests/src/test/java/ru/stqa/pft/addressbook10/model/ContactData.java
+++ addresbook-web-tests/src/test/java/ru/stqa/pft/addressbook10/model/ContactData.java
@@ -3,38 +3,65 @@ package ru.stqa.pft.addressbook10.model;
 import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.*;
 import java.io.File;
 import java.util.Objects;
 
 @XStreamAlias("contact")
+@Entity
+@Table(name = "addressbook")
 public class ContactData {
   @XStreamOmitField
+  @Id
+  @Column(name = "id")
   private int id = Integer.MAX_VALUE;
   @Expose
+  @Column(name = "firstname")
   private String firstname;
   @Expose
+  @Column(name = "lastname")
   private String lastname;
   @Expose
+  @Column(name = "address")
+  @Type( type = "text")
   private String address;
   @Expose
+  @Column(name = "home")
+  @Type( type = "text")
   private String homePhone;
   @Expose
+  @Column(name = "mobile")
+  @Type( type = "text")
   private String mobilePhone;
   @Expose
+  @Column(name = "work")
+  @Type( type = "text")
   private String workPhone;
   @Expose
+  @Column(name = "email")
+  @Type( type = "text")
   private String email;
   @Expose
+  @Column(name = "email2")
+  @Type( type = "text")
   private String email2;
   @Expose
+  @Column(name = "email3")
+  @Type( type = "text")
   private String email3;
   @Expose
+  @Transient  // не будет извлекаться из бд
   private String group;
+  @Transient
   private String allPhones;
+  @Transient
   private String allEmails;
   @Expose
-  private File photo;
+  @Column(name = "photo")
+  @Type( type = "text")
+  private String photo;
 
   public int getId() {
     return id;
@@ -89,7 +116,7 @@ public class ContactData {
   }
 
   public File getPhoto() {
-    return photo;
+    return new File(photo);
   }
 
   public ContactData withId(int id) {
@@ -158,7 +185,7 @@ public class ContactData {
   }
 
   public ContactData withPhoto(File photo) {
-    this.photo = photo;
+    this.photo = photo.getPath();
     return this;
   }
 
